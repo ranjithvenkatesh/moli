309c9c9e8e40ff5762c96794e9fad648a236c5b4
Ranjith Venkatesh <Ranjith.Venkatesh@mossandlichens.com>
2018-12-26 23:16:40 +0100

    Added google login

---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from 'angularx-social-login';
+import { SocialUser } from 'angularx-social-login';
+import { GoogleLoginProvider} from 'angularx-social-login';
+
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.scss']
+})
+export class DemoComponent implements OnInit {
+
+  user: SocialUser;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+}
